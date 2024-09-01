7c47315107d36aeb62d36eadb68981278f671433
Daniel Micay <daniel.micay@grapheneos.org>
2024-09-01 12:53:29 -0400

    fix use-after-free

--- drivers/edgetpu/gcip-kernel-driver/drivers/gcip/gcip-firmware.c
+++ drivers/edgetpu/gcip-kernel-driver/drivers/gcip/gcip-firmware.c
@@ -114,9 +114,9 @@ struct gcip_fw_tracing *gcip_firmware_tracing_create(const struct gcip_fw_tracin
 	fw_tracing->dentry = debugfs_create_dir("fw_tracing", args->dentry);
 	if (IS_ERR(fw_tracing->dentry)) {
 		dev_warn(args->dev, "Failed to create debug FS tracing");
+		struct gcip_fw_tracing *err = (struct gcip_fw_tracing *)fw_tracing->dentry;
 		kfree(fw_tracing);
-
-		return (struct gcip_fw_tracing *)fw_tracing->dentry;
+		return err;
 	}
 
 	debugfs_create_file("active", 0440, fw_tracing->dentry, fw_tracing,
